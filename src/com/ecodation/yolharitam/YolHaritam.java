acae5c9d3a44aeddf43bc1ac888c914eb68a920d
HamitMizrak <hamitmizrak@gmail.com>
2021-04-17 13:32:48 +0300

    yol haritam ve teknolojiler

---
+++ src/com/ecodation/yolharitam/YolHaritam.java
@@ -0,0 +1,24 @@
+package com.ecodation.yolharitam;
+
+public class YolHaritam {
+	/*
+	 * 
+	 * ****FrontEnd*********** Html5 Css3 Js Jquery bootstrap ReactJs VueJs Angular
+	 * 
+	 * ****Backend*********** Java EE Html5 Xhtml Servlet JSP JSF Primefaces Spring Boot
+	 * 
+	 * DB Sql Mysql Mssql Postgresql noSql
+	 * 
+	 * ORM (Hibernate)
+	 * 
+	 * Linux
+	 * 
+	 * Json/XML Servisler (Jax-Rs -Jax-Ws) Servisler (Jax-Rs =Jersey) Servisler (Jax-Ws) axist)
+	 * 
+	 * Micro Servisler
+	 * 
+	 * Docker Node Js
+	 * 
+	 * 
+	 */
+}
